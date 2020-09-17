060dfdfe65e3cfa0c6b3db6a2dca74c178ac25f2
김범준 <rktqjawns@naver.com>
2020-09-17 18:21:53 +0900

    초기 커밋.

---
+++ ConsolePhoneBook/PhoneBookManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePhoneBook
+{
+	public class PhoneBookManager// 외부에서 호출하기위해 public
+	{
+		const int MAX_CNT = 100;
+		PhoneInfo[] infoStorage = new PhoneInfo[MAX_CNT];
+		int curCnt = 0;
+
+		//PhoneInfo phoneInfo = new PhoneInfo();
+
+		public PhoneBookManager()
+		{
+			
+		}
+		public void ShowMenu()
+		{
+			Console.WriteLine("---------------------- 주소록 ------------------------");
+			Console.WriteLine("1. 입력  |  2. 목록  | 3. 검색  | 4.삭제  |  5. 종료");
+			Console.WriteLine("------------------------------------------------------");
+			Console.Write("선택 : ");
+		}
+
+		public void InputData()
+		{
+			
+
+			Console.Write("이름을 입력하세요 : ");
+			name = Console.ReadLine();
+			Console.Write("전화번호를 입력하세요 : ");
+			phoneNumber = Console.ReadLine();
+
+			for (curCnt = 0; curCnt < infoStorage.Length; curCnt++)
+			{
+				infoStorage[curCnt] = new PhoneInfo(name, phoneNumber);
+				
+			}
+		}
+		public void ListData()
+		{
+
+		}
+		public void SearchData()
+		{
+
+		}
+		public void DeleteData()
+		{
+
+		}
+	}
+}
---
+++ ConsolePhoneBook/PhoneInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePhoneBook
+{
+	public class PhoneInfo //외부로 호출하기위해 클래스를 public으로 두는 습관
+	{
+		public string name; //필수
+		public string phoneNumber; //필수
+		public string birth; //선택
+
+		public PhoneInfo() {}
+		public PhoneInfo(string name, string phoneNumber)
+		{
+			this.name = name;
+			this.phoneNumber = phoneNumber;
+		}
+
+		public PhoneInfo(string name, string phoneNumber, string birth)
+		{
+			this.name = name;
+			this.phoneNumber = phoneNumber;
+			this.birth = birth;
+		}
+
+		public void ShowPhoneInfo()
+		{
+			if (birth == null)
+				Console.WriteLine("이름 : {0}\n번호 : {1}\n",name, phoneNumber);
+			else
+				Console.WriteLine("이름 : {0}\n번호 : {1}\n생일 : {2}", name, phoneNumber, birth);
+
+		}
+	}
+
+}
